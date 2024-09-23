db6c85022c7ce152a40de1a584334e167221e7e3
netVJ <vj007@netVJ>
2024-09-23 10:35:47 +0700

    NIDS Update - Suricata

--- Task/4/nids.sh
+++ Task/4/nids.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-echo "Updating system packages..."
-sudo apt-get update
-echo "Installing Snort..."
-sudo apt-get install -y snort
 
-if ! command -v snort &> /dev/null; then
-  echo "Snort could not be installed. Exiting..."
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit
+fi
+
+echo "Installing Suricata..."
+sudo apt-get update
+sudo apt-get install suricata -y
+
+INTERFACE=$(ip link show | awk '/state UP/ {print $2}' | tr -d ':')
+if [ -z "$INTERFACE" ]; then
+  echo "No active network interface found!"
   exit 1
 fi
+echo "Detected active interface: $INTERFACE"
 
-echo "Configuring Snort..."
-RULES_FILE="/etc/snort/rules/local.rules"
+RULES_FILE="/etc/suricata/rules/local.rules"
+echo "Configuring Suricata rules..."
 
-echo 'alert icmp any any -> any any (msg:"ICMP Ping Detected"; sid:1000001; rev:1;)' > $RULES_FILE
-SNORT_CONF="/etc/snort/snort.conf"
+cat > $RULES_FILE << EOL
+# Rule 1: Detect ICMP ping
+alert icmp any any -> any any (msg:"ICMP Ping Detected"; sid:1000001; rev:1;)
 
-if grep -q "include \$RULE_PATH/local.rules" $SNORT_CONF; then
-  echo "Local rules are already included in Snort configuration."
-else
-  echo "include \$RULE_PATH/local.rules" >> $SNORT_CONF
-fi
+# Rule 2: Detect SSH connections
+alert tcp any any -> any 22 (msg:"SSH Connection Detected"; sid:1000002; rev:1;)
 
-echo "Restarting Snort service..."
-systemctl restart snort
+# Rule 3: Detect HTTP traffic
+alert tcp any any -> any 80 (msg:"HTTP Traffic Detected"; sid:1000003; rev:1;)
+EOL
 
-INTERFACE="eth0"
-echo "Running Snort on interface $INTERFACE..."
-snort -A console -q -i $INTERFACE -c /etc/snort/snort.conf
-
-echo "Snort is now running and monitoring traffic on $INTERFACE."
+echo "Running Suricata on interface $INTERFACE..."
+sudo suricata -c /etc/suricata/suricata.yaml -i $INTERFACE
